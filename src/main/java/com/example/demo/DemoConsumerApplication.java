872a00c10b4f038280129872ce1a9fcd8aa793ec
jd68770 <josematias.misiego@kbc.be>
2022-07-19 14:40:41 +0200

    Using Kafka in the cloud via Confluent instead Docker container

--- src/main/java/com/example/demo/DemoConsumerApplication.java
+++ src/main/java/com/example/demo/DemoConsumerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoConsumerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoConsumerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoConsumerApplication.class, args);
+    }
 
 }
